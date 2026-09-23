acd1e1f39805c3150702e14f7ddf63bf9934a855
Nolan Poirier <nolan.poirier@etu.unicaen.fr>
2026-09-23 16:36:13 +0200

    feat: complete card click handling, dynamic image rendering, and asynchronous match validation

--- JS/script.js
+++ JS/script.js
@@ -80,6 +80,14 @@ function resetBoard(){
     lockBoard = false;
 }
 
+function formatTime(sec) {
+    const minutes = Math.floor(sec / 60);
+    const seconds = sec % 60;
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 initGame();
 
 
